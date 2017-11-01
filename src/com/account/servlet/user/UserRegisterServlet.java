ec1f3d21c9a2d25a55d3ad85a58a1ba8084f20c8
NealDor <2656798470@qq.com>
2017-11-01 15:16:52 +0800

    third

--- src/com/account/servlet/user/UserRegisterServlet.java
+++ src/com/account/servlet/user/UserRegisterServlet.java
@@ -1,11 +1,18 @@
 package com.account.servlet.user;
 
+import com.account.entity.UsersEx;
+import com.account.service.UserService;
+import com.account.service.impl.UserServiceImpl;
+import com.google.gson.Gson;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 
 /**
  * 用户注册servlet
@@ -14,11 +21,40 @@ import java.io.PrintWriter;
  */
 public class UserRegisterServlet extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html;charset=utf-8");
         PrintWriter out = response.getWriter();
-        response.sendRedirect(request.getContextPath() + "/page/public/register.html");
+        String userRegisterJson = request.getParameter("userRegisterJson");
+        System.out.println("-----------------" + userRegisterJson);
+        Gson gson = new Gson();
+        UsersEx usersEx = gson.fromJson(userRegisterJson, UsersEx.class);
+        usersEx.setRegisterDate(new Timestamp(System.currentTimeMillis()));
+        System.out.println("++++++++++++++++++" + usersEx.toString());
+        UserService userService = new UserServiceImpl();
+        try {
+            boolean flag1 = userService.existEmail(usersEx);
+            boolean flag2 = userService.existUserName(usersEx);
+            boolean flag3 = userService.existPhoneNum(usersEx);
+            if (flag1) {
+                if (flag2) {
+                    if (flag3) {
+                        out.write("SUCCESS");
+                        userService.saveUser(usersEx);
+                    } else {
+                        out.write("existPhoneNum");
+                    }
+                } else {
+                    out.write("existUserName");
+                }
+            } else {
+                out.write("existEmail");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        out.flush();
+        out.close();
     }
 }
